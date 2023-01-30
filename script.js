e4551afffd073d2b22f667a3e16a09314d696f11
bilalali15 <blt5257@gmail.com>
2023-01-30 19:09:18 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // 0 is rock, 1 is scissors, 2 is paper
 const computerSelection = getComputerChoice();
-let playerSelection = prompt("Choose 0, 1, 2");
 
 function getComputerChoice(){
     let computernum = Math.floor(Math.random() * 3);
@@ -12,25 +11,31 @@ function playround(playerselection, computerselection){
         console.log("Draw");
     }
     else if(playerselection == 0 && computerselection ==1){
-        console.log("win");
+        console.log("Win");
+        return "Win";
     }
     else if(playerselection == 0 && computerselection ==2){
-        console.log("lose");
+        console.log("Lose");
+        return "Lose";
     }
     else if(playerselection == 1 && computerselection ==0){
         console.log("Lose");
+        return "Lose";
     }
     else if(playerselection == 1 && computerselection ==1){
         console.log("Draw");
     }
     else if(playerselection == 1 && computerselection ==2){
         console.log("Win");
+        return "Win";
     }
     else if(playerselection == 2 && computerselection ==0){
         console.log("Win");
+        return "Win";
     }
     else if(playerselection == 2 && computerselection ==1){
         console.log("Lose");
+        return "Lose";
     }
     else if(playerselection == 2 && computerselection ==2){
         console.log("Draw");
@@ -41,19 +46,32 @@ function game(){
     let playerscore = 0;
     let computerscore = 0;
     let result;
+    let playerSelection;
 
-    for(let i = 0; i < 5; i++){
-        result = playround(playerSelection, computerSelection);
-        if(result == "Win"){
-            playerscore++;
-        }
-        else if(result == "Lose"){
-            computerscore++;
-        }
-        else{
-            console.log("Draw");
-        }
+    for(let i = 0; i < 10; i++){
+
+    playerSelection = prompt("Choose 0, 1, 2");
+    result = playround(playerSelection, computerSelection);
+
+    if(result == "Win"){
+        playerscore++;
+    }
+    else if(result == "Lose"){
+        computerscore++;
+    }
+
+    console.log(playerscore);
+    console.log(computerscore);
+
+    if(playerscore == 5 && computerscore < 5){
+        console.log("Player wins!");
+        break;
+    }
+    else if(playerscore < 5 && computerscore == 5){
+        console.log("Computer Wins!");
+        break;
     }
 }
+}
 
-console.log(playround(playerSelection, computerSelection));
+console.log(game());
